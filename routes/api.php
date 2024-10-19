3162216aa8773d8d5e3a8b4dbe6667301130ac1f
jfrem <jfrem181@hotmail.com>
2024-10-19 13:43:28 -0500

    Ajusten header

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,9 @@
 
 require_once __DIR__ . '/../autoload.php';
 
-$allowedOrigins = ['http://localhost:8100', 'http://127.0.0.1:8100'];
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: $origin");
-}
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
